6f08e004e13371703756f5c4a6b4baaaff0e8272
noamgariani11 <noamgariani14@gmail.com>
2024-06-14 21:33:21 -0500

    add rbac for oauth

--- cybercrm/app/controllers/sessions_controller.rb
+++ cybercrm/app/controllers/sessions_controller.rb
@@ -13,6 +13,14 @@ class SessionsController < ApplicationController
       return
     end
 
+    if ['noamgariani@tamu.edu', 'pcr@tamu.edu', 'willtaaa@tamu.edu'].include?(user.email)
+      user.update(role: 'super_user')
+    elsif ['john.romero@tamu.edu', 'pcannell512@tamu.edu', 'chrisvmuniz@tamu.edu'].include?(user.email)
+      user.update(role: 'program_director')
+    else
+      user.update(role: 'student_worker')
+    end
+
     session[:user_id] = user.id
     redirect_to pages_home_path, notice: I18n.t('signed_in')
   end
