de57c56de1aedab5ee00c29894ea6b8291881f4a
noamgariani11 <noamgariani14@gmail.com>
2024-06-27 16:18:42 -0500

    update

--- cybercrm/db/seeds.rb
+++ cybercrm/db/seeds.rb
@@ -16,18 +16,18 @@ require 'faker'
                     {
                       name: "#{Faker::Name.male_first_name} #{Faker::Name.last_name}",
                       uin: Faker::Number.number(digits: 8),
-                      grade_ryg: ['R', 'Y', 'G'].sample,
-                      gender: ['Male', 'Female'].sample,
+                      grade_ryg: %w[R Y G].sample,
+                      gender: %w[Male Female].sample,
                       ethnicity: Faker::Demographic.race,
                       nationality: Faker::Nation.nationality,
                       expected_graduation: Faker::Date.between(from: '2023-08-23', to: '2026-05-23'),
-                      university_classification: ['Freshman', 'Sophmore', 'Junior', 'Senior'].sample,
+                      university_classification: %w[Freshman Sophmore Junior Senior].sample,
                       status: 'Active',
-                      sexual_orientation: ['Heterosexual', 'Homosexual'].sample,
+                      sexual_orientation: %w[Heterosexual Homosexual].sample,
                       date_of_birth: Faker::Date.between(from: '1990-01-01', to: '2001-01-01'),
-                      internships: ['Yes', 'No'].sample,
-                      corps: ['Yes', 'No'].sample,
-                      security_clearance: ['Yes', 'No'].sample
+                      internships: %w[Yes No].sample,
+                      corps: %w[Yes No].sample,
+                      security_clearance: %w[Yes No].sample
                     }
                   ])
 end
