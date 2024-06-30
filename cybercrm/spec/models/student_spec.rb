d8ebdd8ae22adea03369c16fce46b4cfdc8a3a45
Patrick Cannell <pcannell512@tamu.edu>
2024-06-29 21:36:34 -0500

    student model rspec

--- cybercrm/spec/models/student_spec.rb
+++ cybercrm/spec/models/student_spec.rb
@@ -3,5 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe Student do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'Student model is valid with required attributes name and uin' do
+    student = Student.new( name: 'John Doe', uin: 12345678 )
+    expect(student).to be_valid
+  end
+
+  it 'Student model is not valid without a name' do
+    student = Student.new(uin: 12345678)
+    expect(student).to_not be_valid
+    expect(student.errors[:name]).to include("can't be blank")
+  end
+  it 'Student model is not valid without a uin' do
+    student = Student.new(name: 'John Doe')
+    expect(student).to_not be_valid
+    expect(student.errors[:uin]).to include("can't be blank")
+  end
 end
