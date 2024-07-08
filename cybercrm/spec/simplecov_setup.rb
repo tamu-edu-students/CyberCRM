3d6bb4be12f6120149301bff7506e7d6d5577c31
Will <39605195+William-Tatum@users.noreply.github.com>
2024-07-08 03:12:27 -0500

    Update simplecov_setup.rb

--- cybercrm/spec/simplecov_setup.rb
+++ cybercrm/spec/simplecov_setup.rb
@@ -3,8 +3,6 @@
 require 'simplecov'
 require 'simplecov_json_formatter'
 
-SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
-
 SimpleCov.start 'rails' do
   add_filter '/spec/'
   add_filter '/features/'
