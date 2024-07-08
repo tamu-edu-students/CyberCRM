ac8c0ba92478919657c5cb6280af175967ad1ecf
Will <39605195+William-Tatum@users.noreply.github.com>
2024-07-08 02:02:43 -0500

    Update spec_helper.rb

--- cybercrm/spec/spec_helper.rb
+++ cybercrm/spec/spec_helper.rb
@@ -15,17 +15,7 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'simplecov'
-require 'simplecov-json'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::JSONFormatter
-])
-
-SimpleCov.start 'rails' do
-  add_filter '/spec/'
-end
+require_relative 'simplecov_setup'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
