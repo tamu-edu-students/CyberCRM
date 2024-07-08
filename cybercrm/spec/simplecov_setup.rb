76cad5ee7b24239d86980d49006c4789e432290e
Will <39605195+William-Tatum@users.noreply.github.com>
2024-07-08 02:32:46 -0500

    Update simplecov_setup.rb

--- cybercrm/spec/simplecov_setup.rb
+++ cybercrm/spec/simplecov_setup.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'simplecov-json'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::JSONFormatter
-])
 
 SimpleCov.start 'rails' do
   add_filter '/spec/'
