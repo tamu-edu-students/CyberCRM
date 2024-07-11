0ec97a724b4273634c9c83af8b8ad8cfe2b40270
noamgariani11 <noamgariani14@gmail.com>
2024-07-11 15:19:53 -0500

    fix faker data to comply with input validation

--- cybercrm/db/schema.rb
+++ cybercrm/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,46 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# rubocop:disable Metrics/BlockLength
-ActiveRecord::Schema[7.1].define(version: 20_240_711_064_114) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_11_064114) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'students', force: :cascade do |t|
-    t.string 'name'
-    t.string 'uin'
-    t.decimal 'gpa'
-    t.string 'gender'
-    t.string 'ethnicity'
-    t.string 'nationality'
-    t.date 'expected_graduation'
-    t.string 'university_classification'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'status'
-    t.string 'sexual_orientation'
-    t.date 'date_of_birth'
-    t.string 'grade_ryg'
-    t.string 'email'
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "uin"
+    t.decimal "gpa"
+    t.string "gender"
+    t.string "ethnicity"
+    t.string "nationality"
+    t.date "expected_graduation"
+    t.string "university_classification"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.string "sexual_orientation"
+    t.date "date_of_birth"
+    t.string "grade_ryg"
+    t.string "email"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'provider'
-    t.string 'uid'
-    t.string 'name'
-    t.string 'email'
-    t.string 'image'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'role'
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'full_name'
-    t.string 'avatar_url'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "email"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "role"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "full_name"
+    t.string "avatar_url"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
-# rubocop:enable Metrics/BlockLength
