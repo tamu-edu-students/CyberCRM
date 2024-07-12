3924f9b3ce77eb8ccfdb38fc1dbfdf621779f1d1
noamgariani11 <noamgariani14@gmail.com>
2024-07-12 17:10:27 -0500

    color for student grade

--- cybercrm/app/helpers/application_helper.rb
+++ cybercrm/app/helpers/application_helper.rb
@@ -3,6 +3,19 @@
 # This is the application helper
 module ApplicationHelper
   # rubocop:disable Metrics/MethodLength
+  def grade_ryg_class(grade)
+    case grade
+    when 'G'
+      'bg-green-500 text-white'
+    when 'R'
+      'bg-red-500 text-white'
+    when 'Y'
+      'bg-yellow-500 text-black'
+    else
+      ''
+    end
+  end
+
   def sortable(column, title = nil)
     title ||= column.titleize
     current_direction = params[:sort] == column ? params[:direction] : nil
