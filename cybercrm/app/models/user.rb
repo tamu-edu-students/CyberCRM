2d2548860232dec68555186d448d14d19c6fb34d
noamgariani11 <noamgariani14@gmail.com>
2024-07-13 23:21:01 -0500

    fix smell

--- cybercrm/app/models/user.rb
+++ cybercrm/app/models/user.rb
@@ -1,56 +1,53 @@
-# frozen_string_literal: true
-
-# This is user < application record
 class User < ApplicationRecord
-  # Validations
-  # https://guides.rubyonrails.org/testing.html
-
-  # presence ensures that it is not nil
-  validates :provider, :uid, :email, :role, presence: true
-  validates :name, presence: true, length: { minimum: 4, maximum: 200 }
-
-  # uniqueness ensures that it is unique across users
-  validates :email,
-            format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
-
   # Method to find or create a user from OmniAuth data
   # Used ChatGPT to rewrite this for preloading a user
   # the prompt was basically asking it to modify this to work
   # with my user creation and handle the different cases of user state
-  # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/AbcSize
+  validates :provider, :uid, :email, :role, presence: true
+  validates :name, presence: true, length: { minimum: 4, maximum: 200 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+
   def self.from_omniauth(auth)
-    user = where(provider: auth.provider, uid: auth.uid).first_or_initialize
-    existing_user_with_email = User.find_by(email: auth.info.email)
+    user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
 
     if user.persisted?
-      # User exists with provider and uid
-      user.update!(
-        name: auth.info.name,
-        image: auth.info.image,
-        email: auth.info.email
-      )
-    elsif existing_user_with_email
-      # User exists with the same email
+      update_existing_user(user, auth.info)
+    else
+      handle_new_user(user, auth.info)
+    end
+
+    user
+  end
+
+  private
+
+  def self.update_existing_user(user, auth_info)
+    existing_user_with_email = find_by(email: auth_info.email)
+
+    if existing_user_with_email && existing_user_with_email != user
       existing_user_with_email.update!(
-        provider: auth.provider,
-        uid: auth.uid,
-        name: auth.info.name,
-        image: auth.info.image
+        provider: user.provider,
+        uid: user.uid,
+        name: auth_info.name,
+        image: auth_info.image
       )
       user = existing_user_with_email
     else
-      # New user
-      user.assign_attributes(
-        email: auth.info.email,
-        name: auth.info.name,
-        image: auth.info.image,
-        role: 'student_worker'
+      user.update!(
+        name: auth_info.name,
+        image: auth_info.image,
+        email: auth_info.email
       )
-      user.save!
     end
-    user
+  end
+
+  def self.handle_new_user(user, auth_info)
+    user.assign_attributes(
+      email: auth_info.email,
+      name: auth_info.name,
+      image: auth_info.image,
+      role: 'student_worker'
+    )
+    user.save!
   end
 end
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/AbcSize
