2c7a31af077c76ffb6b35201dcb4cd9c14691ced
noamgariani11 <noamgariani14@gmail.com>
2024-07-13 21:49:05 -0500

    update

--- cybercrm/features/support/env.rb
+++ cybercrm/features/support/env.rb
@@ -10,7 +10,7 @@ require 'cucumber/rails'
 require 'capybara/rails'
 require 'mocha/minitest'
 require 'capybara/cucumber'
-require_relative '/cybercrm/spec/simplecov_setup'
+require_relative '../../spec/simplecov_setup'
 require 'rack_session_access/capybara'
 require 'factory_bot'
 
