d30f3a33099418c38fcd209ed6e46fb0aec9569b
Will <39605195+William-Tatum@users.noreply.github.com>
2024-07-14 22:03:02 -0500

    Update 20240714075108_add_unique_indexes.rb

--- cybercrm/db/migrate/20240714075108_add_unique_indexes.rb
+++ cybercrm/db/migrate/20240714075108_add_unique_indexes.rb
@@ -1,8 +1,6 @@
 class AddUniqueIndexes < ActiveRecord::Migration[7.1]
   def change
     add_index :custom_attributes, :name, unique: true
-    add_index :students, :uin, unique: true
-    add_index :students, :email, unique: true
     add_index :users, :email, unique: true
   end
 end
