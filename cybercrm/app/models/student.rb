f73345967f7cc82c279d28d241e46b5af46dc075
William-Tatum <willtaaa@live.com>
2024-07-25 07:19:31 -0500

    I'm literally going insane but it's over enough

--- cybercrm/app/models/student.rb
+++ cybercrm/app/models/student.rb
@@ -6,32 +6,66 @@ class Student < ApplicationRecord
   has_many :options, through: :student_options
   has_many :notes, dependent: :destroy
 
+  before_validation :load_dynamic_options
+
   validates :name, presence: true, length: { minimum: 4, maximum: 200 }
   validates :uin, presence: true, numericality: { only_integer: true }, length: { is: 9 }, uniqueness: true
   validates :grade_ryg, inclusion: { in: %w[G Y R] }
-  validates :gender, presence: true,
-                     inclusion: { in: %w[Male Female] + Option.where(field: 'Gender').pluck(:options) }
-  validates :ethnicity, presence: true,
-                        inclusion: { in: ['Asian', 'Black', 'Hispanic/Latino', 'Native American', 'White', 'Other'] + Option.where(field: 'Ethnicity').pluck(:options) }
-  validates :nationality, presence: true,
-                          inclusion: { in: %w[American British Canadian Australian French German Japanese Chinese Indian Other] + Option.where(field: 'Nationality').pluck(:options) }
+  validates :gender, presence: true, inclusion: { in: ->(student) { student.dynamic_genders } }
+  validates :ethnicity, presence: true, inclusion: { in: ->(student) { student.dynamic_ethnicities } }
+  validates :nationality, presence: true, inclusion: { in: ->(student) { student.dynamic_nationalities } }
   validates :expected_graduation, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
-  validate lambda {
-             if expected_graduation.present? && expected_graduation <= Time.zone.today
-               errors.add(:expected_graduation, 'must be in the future')
-             end
-           }
-  validates :university_classification, presence: true,
-                                        inclusion: { in: %w[Freshman Sophomore Junior Senior Graduate] + Option.where(field: 'University Classification').pluck(:options) }
-  validates :status, presence: true,
-                     inclusion: { in: %w[Active Inactive] + Option.where(field: 'Status').pluck(:options) }
-  validates :sexual_orientation, presence: true,
-                                 inclusion: { in: %w[Heterosexual Homosexual] + Option.where(field: 'Sexual Orientation').pluck(:options) }
+  validate :expected_graduation_must_be_in_the_future
+  validates :university_classification, presence: true, inclusion: { in: ->(student) { student.dynamic_classifications } }
+  validates :status, presence: true, inclusion: { in: ->(student) { student.dynamic_statuses } }
+  validates :sexual_orientation, presence: true, inclusion: { in: ->(student) { student.dynamic_orientations } }
   validates :date_of_birth, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
-  validate lambda {
-             if date_of_birth.present? && date_of_birth > 10.years.ago.to_date
-               errors.add(:date_of_birth, 'must be at least 10 years ago')
-             end
-           }
+  validate :date_of_birth_must_be_at_least_10_years_ago
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+
+  def load_dynamic_options
+    @dynamic_genders = %w[Male Female] + Option.where(field: 'Gender').pluck(:options).flat_map { |opt| opt.split(', ') }
+    @dynamic_ethnicities = ['Asian', 'Black', 'Hispanic/Latino', 'Native American', 'White', 'Other'] + Option.where(field: 'Ethnicity').pluck(:options).flat_map { |opt| opt.split(', ') }
+    @dynamic_nationalities = %w[American British Canadian Australian French German Japanese Chinese Indian Other] + Option.where(field: 'Nationality').pluck(:options).flat_map { |opt| opt.split(', ') }
+    @dynamic_classifications = %w[Freshman Sophomore Junior Senior Graduate] + Option.where(field: 'University Classification').pluck(:options).flat_map { |opt| opt.split(', ') }
+    @dynamic_statuses = %w[Active Inactive] + Option.where(field: 'Status').pluck(:options).flat_map { |opt| opt.split(', ') }
+    @dynamic_orientations = %w[Heterosexual Homosexual] + Option.where(field: 'Sexual Orientation').pluck(:options).flat_map { |opt| opt.split(', ') }
+  end
+
+  def expected_graduation_must_be_in_the_future
+    if expected_graduation.present? && expected_graduation <= Time.zone.today
+      errors.add(:expected_graduation, 'must be in the future')
+    end
+  end
+
+  def date_of_birth_must_be_at_least_10_years_ago
+    if date_of_birth.present? && date_of_birth > 10.years.ago.to_date
+      errors.add(:date_of_birth, 'must be at least 10 years ago')
+    end
+  end
+
+  # Methods to provide the dynamic options for validations
+  def dynamic_genders
+    @dynamic_genders
+  end
+
+  def dynamic_ethnicities
+    @dynamic_ethnicities
+  end
+
+  def dynamic_nationalities
+    @dynamic_nationalities
+  end
+
+  def dynamic_classifications
+    @dynamic_classifications
+  end
+
+  def dynamic_statuses
+    @dynamic_statuses
+  end
+
+  def dynamic_orientations
+    @dynamic_orientations
+  end
 end
