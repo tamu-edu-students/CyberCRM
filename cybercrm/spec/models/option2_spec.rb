aff44f56a462727c859ac685aadfe0cf37009525
noamgariani11 <noamgariani14@gmail.com>
2024-07-25 17:01:10 -0500

    rspec fix

---
+++ cybercrm/spec/models/option2_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe OptionsController, type: :controller do
+    let(:valid_attributes) {
+      { field: 'Gender', display_type: 'dropdown', options: 'Male,Female' }
+    }
+  
+    let(:invalid_attributes) {
+      { field: nil, display_type: nil, options: nil }
+    }
+  
+    describe "POST #create" do
+      context "with valid params" do
+        it "creates a new Option" do
+          expect {
+            post :create, params: { option: valid_attributes }
+            puts "Response status: #{response.status}"
+            puts "Response body: #{response.body}"
+            expect(response.status).to eq(302) # ensure the response status is a redirect
+            expect(response).to redirect_to(options_path) # ensure it redirects to the options list
+          }.to change(Option, :count).by(1)
+        end
+  
+        it "redirects to the options list" do
+          post :create, params: { option: valid_attributes }
+          expect(response).to redirect_to(options_path)
+        end
+      end
+  
+      context "with invalid params" do
+        it "returns a success response (i.e., to display the 'new' template)" do
+          post :create, params: { option: invalid_attributes }
+          expect(response).to be_successful
+        end
+      end
+    end
+  end
--- cybercrm/spec/models/option_spec.rb
+++ cybercrm/spec/models/option_spec.rb
@@ -27,31 +27,6 @@ RSpec.describe OptionsController, type: :controller do
     end
   end
 
-#   describe "POST #create" do
-#     context "with valid params" do
-#       it "creates a new Option" do
-#         expect {
-#           post :create, params: { option: valid_attributes }
-#           puts response.body # print the response body for debugging
-#           expect(response.status).to eq(302) # ensure the response status is a redirect
-#           expect(response).to redirect_to(options_path) # ensure it redirects to the options list
-#         }.to change(Option, :count).by(1)
-#       end
-
-#       it "redirects to the options list" do
-#         post :create, params: { option: valid_attributes }
-#         expect(response).to redirect_to(options_path)
-#       end
-#     end
-
-#     context "with invalid params" do
-#       it "returns a success response (i.e., to display the 'new' template)" do
-#         post :create, params: { option: invalid_attributes }
-#         expect(response).to be_successful
-#       end
-#     end
-#   end
-
   describe "GET #edit" do
     it "returns a success response" do
       get :edit, params: { id: option.to_param }
