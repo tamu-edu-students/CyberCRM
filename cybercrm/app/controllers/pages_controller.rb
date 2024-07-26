1a22ebb5c0505838e49a30e4e825209331985d8a
Matthew-Atanas <matthewatanas@tamu.edu>
2024-07-25 19:39:53 -0500

    finished adding role switching and super user control over it

--- cybercrm/app/controllers/pages_controller.rb
+++ cybercrm/app/controllers/pages_controller.rb
@@ -8,4 +8,20 @@ class PagesController < ApplicationController
   def student_detail; end
   def form; end
   def failure; end
+
+  # before_action :authenticate_user!  # Ensure the user is logged in
+  def update_role
+    # logger.debug "Params: #{params.inspect}"  # Add this line to debug parameters
+    new_role = params[:role]
+    sanitized_role_list = current_user.role_list.compact_blank # Remove any empty strings
+
+    if sanitized_role_list.include?(new_role)
+      current_user.update(role: new_role)
+      flash[:notice] = t('role_updated')
+    else
+      flash[:alert] = t('role_alert')
+    end
+    # redirect_to root_path  # Redirect to the homepage or another relevant path
+    redirect_back(fallback_location: root_path)
+  end
 end
--- cybercrm/config/routes.rb
+++ cybercrm/config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
   get '/auth/failure', to: redirect('/')
   get '/logout', to: 'sessions#destroy'
   post 'search_students', to: 'students#search'
+  post 'update_role', to: 'pages#update_role', as: 'update_role'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
