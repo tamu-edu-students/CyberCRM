b16c0141391ed923bfd9f0316b65a76726f28ab4
Christopher Muniz <chrisvmuniz@tamu.edu>
2024-07-27 00:39:19 -0500

    Added everything but still missing cucumber tests. Sorting and filtering work

--- cybercrm/app/controllers/notes_controller.rb
+++ cybercrm/app/controllers/notes_controller.rb
@@ -2,25 +2,40 @@
 
 # Controls notes. params[:id] is the id of the student
 class NotesController < ApplicationController
-  # before_action :set_note, only: %i[show edit update destroy]
-
-  # GET /notes or /notes.json
-  # def index
-  #   @notes = Note.all
-  # end
-
   # GET /notes/1 or /notes/1.json
   def show
     @student = Student.find(params[:id])
     Rails.logger.debug @student.name
     @notes = @student.notes
-    Rails.logger.debug @notes
+
+    if params[:note_created_date].present?
+      @notes = @notes.where(note_created_date: params[:note_created_date])
+    end
+    if params[:note].present?
+      @notes = @notes.where("note LIKE ?", "%#{params[:note]}%")
+    end
+    if params[:followup_date].present?
+      @notes = @notes.where(followup_date: params[:followup_date])
+    end
+    if params[:followup_action].present?
+      @notes = @notes.where("followup_action LIKE ?", "%#{params[:followup_action]}%")
+    end
+    if params[:action_completed].present?
+      @notes = @notes.where(action_completed: params[:action_completed])
+    end
+
+    sort_column = params[:sort] || 'note_created_date'
+    sort_direction = params[:direction] || 'asc'
+    @notes = @notes.order("#{sort_column} #{sort_direction}")
   end
 
   # GET /notes/new
   def new
     @note = Note.new
   end
+end
+
+
 
   # GET /notes/1/edit
   # def edit; end
@@ -75,4 +90,4 @@ class NotesController < ApplicationController
   # def note_params
   #   params.fetch(:note, {})
   # end
-end
+
